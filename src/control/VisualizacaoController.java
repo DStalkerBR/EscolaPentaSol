72c7dc29573c2be17b0c3948a52f2f0eecd97118
DStalkerBR <34558728+DStalkerBR@users.noreply.github.com>
2018-06-13 12:07:00 -0300

    Update VisualizacaoController.java

--- src/control/VisualizacaoController.java
+++ src/control/VisualizacaoController.java
@@ -1,1 +1,41 @@
 package control;
+
+import java.util.ArrayList;
+import model.entidade.VisualizacaoInstrumentos;
+import model.entidade.VisualizacaoAdministradores;
+import model.entidade.VisualizacaoProfessores;
+import model.entidade.VisualizacaoAlunos;
+import java.util.List;
+import model.bean.Administrador;
+import model.bean.Aluno;
+import model.bean.Instrumento;
+import model.bean.Professor;
+
+public class VisualizacaoController {
+    
+    public List<Aluno> visualizarAlunos() {
+        return VisualizacaoAlunos.listar();
+    }
+    
+    
+    public List<Professor> visualizarProfessor() {
+        return VisualizacaoProfessores.listar();
+    }
+    
+    public List<Administrador> visualizarAdministrador() {
+        return VisualizacaoAdministradores.listar();
+    }
+    
+    public List<Instrumento> visualizarInstrumento() {
+        return VisualizacaoInstrumentos.listar();
+    }
+    
+    //Teste
+    /*public static void main(String argv[]){
+        VisualizacaoController visualiza = new VisualizacaoController();
+        List<Instrumento> lista = new ArrayList<>(visualiza.visualizarInstrumento());
+        lista.forEach((instrumento) -> {
+               System.out.println("ID" + instrumento.getId() + "NOME" + instrumento.getNome() + "DESC" + instrumento.getDescricao() + " QNT "+instrumento.getQtdInstrumentos());
+         });         
+    } */
+}
